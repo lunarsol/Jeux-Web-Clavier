75024fd6e79c99c23710ba269bcbafb377a7bc3f
fablab <lunarsol@duck.com>
2024-01-23 15:36:37 +0100

    Bug fix : - Round limit option added - Code size reduction
    Not tested yet

--- script.js
+++ script.js
@@ -1,75 +1,70 @@
-const mots = ["tekno", "musique", "pomme"]
-const phrase = ["La tekno c'est vraiment cool", "J'adore la vie", "La musique est poetique"]
-let points = 0
+const mots = ["tekno", "musique", "pomme"];
+const phrase = ["La tekno c'est vraiment cool", "J'adore la vie", "La musique est poétique"];
+let points = 0;
 
-function score(){
-    return`Votre Score est de ${points} points`
+function score() {
+    return `Votre Score est de ${points} points`;
 }
-function scoreMax(chiffre) {
-    alert(`Vous voulez atteindre ${chiffre} points`);
-    let limite = prompt("Voulez-vous un nombre maximal de tours ? y/n");
 
-    while (limite !== "y" && limite !== "n") {
-        alert("Vous devez entrer 'y' ou 'n'");
-        limite = prompt("Voulez-vous un nombre maximal de tours ? y/n");
-    }
-
-    if(limite === "y"){
-        return parseInt(prompt("Entre un nombre pour definir la limite "))
-    }else if (limite === "n"){
-        alert("Tres bien vous n'aurez pas de limite !")
-        return Infinity
+function limiteBoucleMax(scoreLimite) {
+    if (scoreLimite === "y") {
+        let limite = parseInt(prompt("Combien de manches voulez-vous ?"));
+        if (!isNaN(limite) && limite > 0) {
+            return limite;
+        } else {
+            alert("Vous devez entrer un nombre positif.");
+        }
+    } else if (scoreLimite === "n") {
+        alert("Très bien, vous n'aurez pas de limite !");
+        return Infinity;
     } else {
-        alert("Vous devez rentrer 'y' ou 'n'")
+        alert("Vous devez rentrer 'y' ou 'n'");
+        // Vous pouvez ajouter une logique pour gérer une nouvelle tentative de saisie si nécessaire.
     }
 }
-let scoreStop = scoreMax()
+
+let boucleStop = limiteBoucleMax(prompt("Voulez-vous une limite de score ? y/n"));
 
 function modeDeJeu(choix) {
     while (choix !== 1 && choix !== 2) {
         if (choix === 1) {
-            alert("Vous avez choisi le mode mot")
+            alert("Vous avez choisi le mode mot");
         } else if (choix === 2) {
-            alert("Vous avez choisis le mode phrase")
+            alert("Vous avez choisi le mode phrase");
         } else {
-            alert("Le choix n'est pas bon")
+            alert("Le choix n'est pas bon");
         }
     }
-    return choix
 }
-let choixMode = modeDeJeu()
 
-if (choixMode === 1 || choixMode === 2) {
-    if (scoreStop === Infinity || typeof scoreStop === 'number') {
-        while (points < scoreStop) {
-            let question, reponse;
+let choixMode = prompt("Voulez-vous jouer avec des phrases (tapez 2) ou avec des mots (tapez 1)");
+choixMode = Number(choixMode);
+modeDeJeu(choixMode);
 
-            if (choixMode === 1) {
-                question = `Le mot à réécrire est ${mots[points]}`;
-                reponse = prompt(question);
-            } else if (choixMode === 2) {
-                question = `La phrase à réécrire est ${phrase[points]}`;
-                reponse = prompt(question);
-            }
+if (Number(choixMode) === 1 || Number(choixMode) === 2) {
+    let toursEffectues = 0; // Ajout de cette variable pour compter les tours
 
-            if (reponse === (choixMode === 1 ? mots[points] : phrase[points])) {
-                points += 1;
-            }
+    while (toursEffectues < boucleStop) {
+        let question, reponse;
 
-            alert(`Votre score est de ${score()}`);
+        if (Number(choixMode) === 1) {
+            question = `Le mot à réécrire est ${mots[points]}`;
+            reponse = prompt(question);
+        } else if (Number(choixMode) === 2) {
+            question = `La phrase à réécrire est ${phrase[points]}`;
+            reponse = prompt(question);
         }
-    } else {
-        alert("scoreStop invalide");
+
+        if (reponse === (Number(choixMode) === 1 ? mots[points] : phrase[points])) {
+            points += 1;
+        }
+
+        toursEffectues += 1; // Incrémentation du nombre de tours effectués
+
+        alert(`Votre score est de ${score()}`);
     }
+
+    // Si vous atteignez ce point, cela signifie que la boucle s'est terminée car le nombre de tours est atteint.
 } else {
     alert("choixMode invalide");
-}
-
-
-
-while (points<3){
-    let reponse=prompt(`Le mot a ecrire est ${mots[points]}`)
-    if (reponse === mots[points]){
-        points+=1
-    }
 }
